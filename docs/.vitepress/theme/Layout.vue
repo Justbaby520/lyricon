d3b9a11f52f0f082746d197ef2ab6462e31a47ab
Proify <247336249+proify@users.noreply.github.com>
2026-05-03 11:00:59 +0800

    refactor(theme): 替换默认布局并优化表格格式
    - 使用自定义Layout组件替代默认主题布局
    - 添加自定义CSS样式文件
    - 修复README中表格格式对齐问题
    - 关闭Xposed模块调试模式

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,15 +1,18 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const { Layout } = DefaultTheme
+const copyright = ref('')
 
 async function fetchWallpaper() {
   try {
     const res = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
     const data = await res.json()
-    const url = `https://www.bing.com${data.images[0].url}`
+    const img = data.images[0]
+    const url = `https://www.bing.com${img.urlbase}_UHD.jpg`
     document.documentElement.style.setProperty('--vp-home-bg-image', `url(${url})`)
+    copyright.value = img.copyright
   } catch {
     document.documentElement.style.setProperty('--vp-home-bg-image', 'none')
   }
@@ -20,4 +23,5 @@ onMounted(fetchWallpaper)
 
 <template>
   <Layout />
+  <div v-if="copyright" class="wallpaper-credit">{{ copyright }}</div>
 </template>
